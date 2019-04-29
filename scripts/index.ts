3936c391ea6d9b04d762500a2e88d12762046060
Nishkal <kashyapnishkal@gmail.com>
2019-04-29 15:52:49 +0530

    initial commit

---
+++ scripts/index.ts
@@ -0,0 +1,179 @@
+/**
+ * Script will create json data, which contains dictionaries for runtime,
+ * where `key` is name of folder we want icon for and `value` is icon's filename.
+ */
+import * as log4js from "log4js";
+import { readFileSync, createWriteStream } from 'fs';
+import { basename, extname } from "path"
+import * as fss from 'fs-extra';
+
+const log = log4js.getLogger(basename(__filename, extname(__filename)));
+log.level = "debug";
+
+// Open icon definitions
+log.debug("opening icon definitions");
+
+// const iconsJSONFile = readFileSync('./node_modules/vscode-icons-js/data/generated/icons.json');
+const iconsJSONFile = readFileSync('./scripts/material-icons.json');
+const vsiLanguagesFile = readFileSync('./node_modules/vscode-icons-js/data/generated/languages-vsi.json');
+const vscodeLanguagesFile = readFileSync('./node_modules/vscode-icons-js/data/static/languages-vscode.json');
+const PATH_ICONSDATA = './src/generated/';
+type IconKey = string;
+
+// Parse icon definitions
+log.debug("parsing icon definitions");
+const icons = JSON.parse(iconsJSONFile.toString()) as {
+    iconDefinitions: { [iconKey: string]: { iconPath: string } },
+    folderNames: { [folderName: string]: IconKey },
+    fileExtensions: { [fileExtension: string]: IconKey },
+    fileNames: { [fileName: string]: IconKey },
+    languageIds: { [language: string]: IconKey }
+    light: {
+        folderNames: { [folderName: string]: IconKey },
+        fileExtensions: { [fileExtension: string]: IconKey },
+        fileNames: { [fileName: string]: IconKey },
+        languageIds: { [language: string]: IconKey }
+    }
+};
+
+icons.light.folderNames = Object.assign(icons.folderNames, icons.light.folderNames);
+icons.light.fileExtensions = Object.assign(icons.fileExtensions, icons.light.fileExtensions);
+icons.light.fileNames = Object.assign(icons.fileNames, icons.light.fileNames);
+icons.light.languageIds = Object.assign(icons.languageIds, icons.light.languageIds);
+
+const vsiLanguages = JSON.parse(vsiLanguagesFile.toString()) as {
+    [language: string]: {
+        ids: string | string[];
+        defaultExtension: string;
+    }
+};
+
+const vscodeLanguages = JSON.parse(vscodeLanguagesFile.toString()) as {
+    [languageId: string]: {
+        extensions: string[],
+        filenames?: string[]
+    }
+};
+
+// Icon To Path
+const iconToPath: { [icon: string]: string } = Object.keys(icons.iconDefinitions).reduce((acc, icon) => ({
+    ...acc,
+    [icon]: icons.iconDefinitions[icon].iconPath.split('/').pop()
+}), {});
+
+
+function getIconPath(icon: string) {
+    return iconToPath[icon];
+}
+
+
+
+// FolderNames to Icon
+(async function () {
+    log.debug("creating table for foldernames");
+    fss.ensureFileSync(PATH_ICONSDATA + 'FolderNamesToIcon.ts');
+    const folderIcons = createWriteStream(PATH_ICONSDATA + 'FolderNamesToIcon.ts', { flags: 'w' });
+    folderIcons.write(`export const FolderNamesToIcon: { [key: string]: string } = {\n`);
+    for (const [folderName, icon] of Object.entries(icons.light.folderNames)) {
+        folderIcons.write(`\t'${folderName}': '${getIconPath(icon)}',\n`);
+    }
+    folderIcons.write("};\n");
+    folderIcons.end();
+})();
+
+// FileExtensions to Icon
+(async function () {
+    log.debug("creating table for filextensions");
+    fss.ensureFileSync(PATH_ICONSDATA + 'FileExtensions1ToIcon.ts');
+    fss.ensureFileSync(PATH_ICONSDATA + 'FileExtensions2ToIcon.ts');
+    const fileExtensions1 = createWriteStream(PATH_ICONSDATA + 'FileExtensions1ToIcon.ts', { flags: 'w' });
+    const fileExtensions2 = createWriteStream(PATH_ICONSDATA + 'FileExtensions2ToIcon.ts', { flags: 'w' });
+    fileExtensions1.write(`export const FileExtensions1ToIcon: { [key: string]: string } = {\n`);
+    fileExtensions2.write(`export const FileExtensions2ToIcon: { [key: string]: string } = {\n`);
+    for (const [extension, icon] of Object.entries(icons.light.fileExtensions)) {
+        if (extension.indexOf(".") === -1) {
+            fileExtensions1.write(`\t'${extension}': '${getIconPath(icon)}',\n`);
+        } else {
+            fileExtensions2.write(`\t'${extension}': '${getIconPath(icon)}',\n`)
+        }
+
+    }
+    fileExtensions1.write("};\n");
+    fileExtensions2.write("};\n");
+    fileExtensions1.end();
+    fileExtensions2.end();
+})();
+
+// FileNames to Icon
+(async function () {
+    log.debug("creating table for filenames");
+    const alreadyIncludedNames: string[] = [];
+    fss.ensureFileSync(PATH_ICONSDATA + 'FileNamesToIcon.ts');
+    const filenames = createWriteStream(PATH_ICONSDATA + 'FileNamesToIcon.ts', { flags: 'w' });
+    filenames.write(`export const FileNamesToIcon: { [key: string]: string } = {\n`);
+    // get folder names from vsi langauges definitions
+    for (const [filename, icon] of Object.entries(icons.light.fileNames)) {
+        filenames.write(`\t'${filename}': '${getIconPath(icon)}',\n`);
+        alreadyIncludedNames.push(filename);
+    }
+    // get folder names from vscode languages definitions
+    for (const [language, data] of Object.entries(vscodeLanguages)) {
+        if (data.filenames && Object.keys(vsiLanguages).includes(language) && getIconPath(`${language}`)) {
+            for (const filename of data.filenames) {
+                if (!alreadyIncludedNames.includes(filename)) {
+                    filenames.write(`\t'${filename}': '${getIconPath(`${language}`)}',\n`);
+                    alreadyIncludedNames.push(filename);
+                }
+            }
+        }
+    }
+    filenames.write("};\n");
+    filenames.end();
+})();
+
+// Languages to Icon
+(async function () {
+    log.debug("creating table for languages");
+    const alreadyIncludedLangs: string[] = [];
+    fss.ensureFileSync(PATH_ICONSDATA + 'LanguagesToIcon.ts');
+    const languages = createWriteStream(PATH_ICONSDATA + 'LanguagesToIcon.ts', { flags: 'w' });
+    languages.write(`export const LanguagesToIcon: { [key: string]: string } = {\n`);
+    for (const [language, icon] of Object.entries(vsiLanguages)) {
+        const iconFileName = icons.languageIds[language]
+        if (iconFileName) {
+            // const withoutPrefix = iconFileName.slice(3); // remove prefix "_f_";
+            const withoutPrefix = iconFileName; // remove prefix "_f_";
+            const lightIconFilename = `${withoutPrefix}`;
+            const existsLightTheme = icons.iconDefinitions[lightIconFilename]; // try to find light theme of icon
+            const iconPath = existsLightTheme
+                ? getIconPath(lightIconFilename)
+                : getIconPath(iconFileName);
+
+            // Are there any language extensions supported by vscode ?
+            if (vscodeLanguages[language]) {
+                const supportedExtensions = vscodeLanguages[language].extensions;
+                const languageExtensions: { [ext: string]: string } = {};
+                for (const extension of supportedExtensions) {
+                    languageExtensions[extension.slice(1)] = iconPath; // .cpp => cpp
+                };
+                // Override default extension
+                // languageExtensions[icon.defaultExtension] = iconPath;
+
+                for (const [extIcon, extIconPath] of Object.entries(languageExtensions)) {
+                    if (!alreadyIncludedLangs.includes(extIcon)) {
+                        languages.write(`\t'${extIcon}': '${extIconPath}',\n`);
+                        alreadyIncludedLangs.push(extIcon);
+                    }
+                }
+            } else {
+                if (!alreadyIncludedLangs.includes(icon.defaultExtension)) {
+                    languages.write(`\t'${icon.defaultExtension}': '${iconPath}',\n`);
+                    alreadyIncludedLangs.push(icon.defaultExtension);
+                }
+            }
+        }
+        // languages.write(`\t"${filename}": "${getIconPath(icon)}",\n`);
+    }
+    languages.write("};\n");
+    languages.end();
+})();
